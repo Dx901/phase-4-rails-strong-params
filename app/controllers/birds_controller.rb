55695c72c0486be58dc8b6c77253b859d6ceb976
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-11-22 07:41:59 +0300

    DONE

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,4 +22,19 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(params.permit(:name, :species))
+    render json: bird, status: :created
+  end
+
+  # def create
+  #   bird = Bird.create(name: params[:name], species: params[:species])
+  #   render json: bird, status: :created
+  # end
+
+  # def create
+  #   bird = Bird.create(params)
+  #   render json: bird, status: :created
+  # end
+
 end
